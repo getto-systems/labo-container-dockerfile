76f7af17a59840d560853259f85e2d785e747f3e
shun <shun.fix9@gmail.com>
2019-07-16 18:41:03 +0000

    fix: env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 groupmod -n $LABO_USER $BUILD_LABO_USER
 usermod -l $LABO_USER -d /home/$LABO_USER -g $LABO_USER -G $LABO_USER,docker $BUILD_LABO_USER
 
+export USER=$LABO_USER
+export HOME=/home/$USER
+
 exec setpriv --reuid $LABO_USER --regid $LABO_USER --init-groups "$@"
