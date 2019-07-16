f76a6adba7076b5168c7193038c9d2615787a5b9
shun <shun.fix9@gmail.com>
2019-07-16 17:47:14 +0000

    fix: use setpriv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 groupmod -n $LABO_USER $BUILD_LABO_USER
-usermod -l $LABO_USER -d /home/$LABO_USER -g $LABO_USER -G $LABO_USER,sudo,docker $BUILD_LABO_USER
+usermod -l $LABO_USER -d /home/$LABO_USER -g $LABO_USER -G $LABO_USER,docker $BUILD_LABO_USER
 
-exec sudo -E -H -u $LABO_USER "$@"
+exec setpriv --reuid $LABO_USER --regid $LABO_USER --init-groups "$@"
