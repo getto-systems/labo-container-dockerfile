6669f277e334bf0db5fa72efedd3f78776fe0ad6
shun <shun.fix9@gmail.com>
2019-07-20 17:50:13 +0000

    fix: move HOME definition to test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+export HOME=$(pwd)
+
 docker build -t getto/labo-container:${CI_COMMIT_SHORT_SHA} . && \
 dockle --exit-code 1 getto/labo-container:${CI_COMMIT_SHORT_SHA} && \
 trivy --exit-code 1 --quiet --ignore-unfixed --auto-refresh getto/labo-container:${CI_COMMIT_SHORT_SHA} && \
