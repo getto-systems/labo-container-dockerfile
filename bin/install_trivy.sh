328efd26e1405430f44fff391a15bd1b1e5b334f
shun <shun.fix9@gmail.com>
2019-07-20 16:22:45 +0000

    fix: gitlab-ci

---
+++ bin/install_trivy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VERSION=$(
+  curl --silent "https://api.github.com/repos/knqyf263/trivy/releases/latest" | \
+  grep '"tag_name":' | \
+  sed -E 's/.*"v([^"]+)".*/\1/' \
+) && curl -L -o trivy.tar.gz https://github.com/knqyf263/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz && tar zxvf trivy.tar.gz && mv trivy /usr/bin
