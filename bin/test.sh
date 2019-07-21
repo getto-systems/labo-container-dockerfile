06ccaf5bbafddd280a9fcbac8c30fddd06718858
shun <shun.fix9@gmail.com>
2019-07-21 15:57:12 +0000

    fix: scripts

--- bin/test.sh
+++ bin/test.sh
@@ -4,7 +4,9 @@ set -x
 
 export HOME=$(pwd)
 
-docker build -t getto/labo-container:${CI_COMMIT_SHORT_SHA} . && \
-dockle --exit-code 1 getto/labo-container:${CI_COMMIT_SHORT_SHA} && \
-trivy --exit-code 1 --quiet --ignore-unfixed --auto-refresh getto/labo-container:${CI_COMMIT_SHORT_SHA} && \
+image=getto/labo-container
+
+docker build -t $image:${CI_COMMIT_SHORT_SHA} . && \
+dockle --exit-code 1 $image:${CI_COMMIT_SHORT_SHA} && \
+trivy --exit-code 1 --quiet --ignore-unfixed --auto-refresh $image:${CI_COMMIT_SHORT_SHA} && \
 :
