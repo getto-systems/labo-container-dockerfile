42a31c7505d87f71956f69ce9b2e6f5733d7199f
shun <shun.fix9@gmail.com>
2019-08-22 20:50:32 +0000

    fix: trivy github path

--- bin/install_trivy.sh
+++ bin/install_trivy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 VERSION=$(
-  curl --silent "https://api.github.com/repos/knqyf263/trivy/releases/latest" | \
+  curl --silent "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | \
   grep '"tag_name":' | \
   sed -E 's/.*"v([^"]+)".*/\1/' \
 ) && \
-curl -L -o trivy.tar.gz https://github.com/knqyf263/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz && \
+curl -L -o trivy.tar.gz https://github.com/aquasecurity/trivy/releases/download/v${VERSION}/trivy_${VERSION}_Linux-64bit.tar.gz && \
 tar zxvf trivy.tar.gz && \
 mv trivy /usr/bin && \
 :
