7812f9bb30baaf3a57d77fc93f9ec68c5bd0deb1
shun <shun.fix9@gmail.com>
2019-08-26 15:34:07 +0000

    fix: gitlab-ci

--- bin/append_date_to_release_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i -e "s|$|-$(date +%Y%m%d%H%M%S)|" .release-version
--- bin/install_ci_tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apk -Uuv add bash git curl tar sed grep && \
-./bin/install_dockle.sh && \
-./bin/install_trivy.sh && \
-:
