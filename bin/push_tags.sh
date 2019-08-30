78831537e8e06227d8a4cccd54390622aa73f27f
shun <shun.fix9@gmail.com>
2019-08-30 11:25:10 +0000

    fix: gitlab-ci

--- bin/push_tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git remote add super https://gett-systems:$GITLAB_ACCESS_TOKEN@gitlab.com/getto-systems-labo/container/dockerfile.git
-git remote add backup https://getto-systems:$GITHUB_ACCESS_TOKEN@github.com/getto-systems/labo-container-dockerfile.git
-git tag $(cat .release-version)
-git push super HEAD:master --tags
-git push super HEAD:update
-git push backup HEAD:master --tags
